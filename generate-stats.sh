73a2377977cb19e1c0068c23b8d1b15d999983e0
Michael Hale <mail@michaelhale.xyz>
2021-04-05 11:58:46 -0400

    Remove S3 archive pulls

--- generate-stats.sh
+++ generate-stats.sh
@@ -3,14 +3,19 @@
 S3_LOGS_ARCHIVE=s3://blaseball-archive-iliana/
 S3_BLASEBALL_REF_ARCHIVE=s3://blaseball-reference/public/json-data/
 
-mkdir -p ./data ./blaseball-logs ./tmp
+# Add temporary status file
+touch ./ACTIVE_SCRIPT_RUN
 
-echo "Pulling game update logs from S3..."
-/usr/local/bin/aws --quiet --no-sign-request s3 sync $S3_LOGS_ARCHIVE ./blaseball-logs/ --exclude "hourly/*" --exclude "compressed-hourly/*" --exclude "idols/*" --exclude "v2/*"
+mkdir -p ./data
 
-echo "Combining logs..."
-cat ./blaseball-logs/*.gz > ./tmp/combined-blaseball-log.json.gz
-gunzip -c ./tmp/combined-blaseball-log.json.gz > ./tmp/blaseball-log.json
+# mkdir -p ./data ./blaseball-logs ./tmp
+
+# echo "Pulling game update logs from S3..."
+# aws --no-sign-request s3 sync s3://blaseball-archive-iliana/ ./blaseball-logs/ --exclude "hourly/*" --exclude "compressed-hourly/*" --exclude "idols/*" --exclude "v2/*"
+
+# echo "Combining logs..."
+# cat ./blaseball-logs/*.gz > ./tmp/combined-blaseball-log.json.gz
+# gunzip -c ./tmp/combined-blaseball-log.json.gz > ./tmp/blaseball-log.json
 
 echo "Pulling latest script updates..."
 git pull
@@ -41,12 +46,15 @@ node dist/generateSchedules.js
 # echo "Generating stat leaders..."
 # node dist/generateStatLeaders.js
 
-echo "Copying generated data to Blaseball Reference S3 bucket..."
-find ./data/ -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
-/usr/local/bin/s3cmd sync --quiet --no-mime-magic --recursive --acl-public --no-preserve --add-header="Content-Type: application/json" --add-header="Content-Encoding: gzip"  --add-header="Cache-Control: max-age=120" ./data/ $S3_BLASEBALL_REF_ARCHIVE
-find ./data/ -type f -exec mv "{}" "{}.gz" \; -exec gunzip "{}" \;
+# echo "Copying generated data to Blaseball Reference S3 bucket..."
+# find ./data/ -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
+# /usr/local/bin/s3cmd sync --quiet --no-mime-magic --recursive --acl-public --no-preserve --add-header="Content-Type: application/json" --add-header="Content-Encoding: gzip"  --add-header="Cache-Control: max-age=120" ./data/ $S3_BLASEBALL_REF_ARCHIVE
+# find ./data/ -type f -exec mv "{}" "{}.gz" \; -exec gunzip "{}" \;
 
-echo "Cleaning up..."
-rm -r ./tmp/
+# echo "Cleaning up..."
+# rm -r ./tmp/
+
+# Remove temporary status file
+rm ./ACTIVE_SCRIPT_RUN
 
 echo "Done!"
